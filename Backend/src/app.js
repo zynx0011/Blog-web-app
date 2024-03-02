1e26a9d239bcc65b0e1b95356e00a4ef6992adf6
zynx0011 <kunalnachan10@gmail.com>
2024-03-02 12:33:44 +0530

    change cros

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,30 +1,30 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 
 const app = express();
 
 // cross origin resource sharing  - it will check the frontend with port
 app.use(
   cors({
-    origin: "https://blog-app-frontend-amber.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend", "dist", "index  .html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "Frontend", "dist", "index  .html"));
+// });
 
 //routes import
 import userRouter from "./routes/user.routes.js";
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": `{window.location.origin}`,
+      "/api": "http://localhost:8000",
     },
   },
   plugins: [react()],
