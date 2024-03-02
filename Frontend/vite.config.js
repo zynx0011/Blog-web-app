441b89e5020c259926ef679810d8bacc6983001a
zynx0011 <kunalnachan10@gmail.com>
2024-03-03 00:06:33 +0530

    added url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://blog-app-backend-c9w1.onrender.com",
     },
   },
   plugins: [react()],
